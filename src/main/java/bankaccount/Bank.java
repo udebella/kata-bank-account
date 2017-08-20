f7b1aceb4fc57d4cee004da01a3ab5ad90540b4e
Ubu <ubald2b@gmail.com>
2017-08-20 23:26:01 +0200

    [#1][NEW] Handle multiple different accounts

--- src/main/java/bankaccount/Bank.java
+++ src/main/java/bankaccount/Bank.java
@@ -1,10 +1,18 @@
 package bankaccount;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Bank {
-    private final Account account = new Account();
+    private final Map<AccountNumber, Account> accounts = new HashMap<>();
 
     public Account makeDeposit(AccountNumber accountNumber, Amount amount) {
+        Account account = accounts.get(accountNumber);
         account.deposit(amount);
         return account;
+    }
+
+    public void addAccount(AccountNumber accountNumber) {
+        accounts.put(accountNumber, new Account());
     }
 }
--- src/test/java/bankaccount/BankDepositTest.java
+++ src/test/java/bankaccount/BankDepositTest.java
@@ -8,41 +8,52 @@ import static bankaccount.AccountNumber.AccountNumberBuilder;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BankDepositTest {
+    private AccountNumberBuilder accountNumberBuilder;
+    private AmountBuilder amountBuilder;
     private Bank bank;
 
     @Before
     public void setUp() {
+        accountNumberBuilder = new AccountNumberBuilder();
+        amountBuilder = new AmountBuilder();
+
         bank = new Bank();
+        bank.addAccount(accountNumberBuilder.withNumber(1).build());
+        bank.addAccount(accountNumberBuilder.withNumber(2).build());
     }
 
     @Test
     public void deposit_should_update_account_balance() {
-        final Amount amount = new AmountBuilder()
-                .withValueAsCents(10)
-                .build();
-        final AccountNumber accountNumber = new AccountNumberBuilder()
-                .withNumber(1)
-                .build();
+        final Amount amount = amountBuilder.withValueAsCents(10).build();
+        final AccountNumber accountNumber = accountNumberBuilder.withNumber(1).build();
 
         Account account = bank.makeDeposit(accountNumber, amount);
 
         assertThat(account.accountBalance())
-                .isEqualTo(new AmountBuilder().withValueAsCents(10).build());
+                .isEqualTo(amountBuilder.withValueAsCents(10).build());
     }
 
     @Test
     public void depositing_two_times_in_a_row_on_the_same_account_should_update_the_account() {
-        final Amount amount = new AmountBuilder()
-                .withValueAsCents(10)
-                .build();
-        final AccountNumber accountNumber = new AccountNumberBuilder()
-                .withNumber(1)
-                .build();
+        final Amount amount = amountBuilder.withValueAsCents(10).build();
+        final AccountNumber accountNumber = accountNumberBuilder.withNumber(1).build();
 
         bank.makeDeposit(accountNumber, amount);
         Account account = bank.makeDeposit(accountNumber, amount);
 
         assertThat(account.accountBalance())
-                .isEqualTo(new AmountBuilder().withValueAsCents(20).build());
+                .isEqualTo(amountBuilder.withValueAsCents(20).build());
+    }
+
+    @Test
+    public void should_manage_multiple_different_accounts() {
+        final Amount amount = amountBuilder.withValueAsCents(10).build();
+        final AccountNumber accountNumber = accountNumberBuilder.withNumber(1).build();
+        final AccountNumber accountNumber2 = accountNumberBuilder.withNumber(2).build();
+
+        Account account = bank.makeDeposit(accountNumber, amount);
+        Account account2 = bank.makeDeposit(accountNumber2, amount);
+
+        assertThat(account).isNotEqualTo(account2);
     }
 }
