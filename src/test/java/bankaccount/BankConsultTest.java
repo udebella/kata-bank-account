78c95893d181f09a83c0d09ec86640f4100702da
Ubald de Bellabre <ubald.de-bellabre@lcdlv.fr>
2017-08-21 16:40:56 +0200

    [#3][ENH] Readability

--- src/test/java/bankaccount/BankConsultTest.java
+++ src/test/java/bankaccount/BankConsultTest.java
@@ -13,19 +13,22 @@ public class BankConsultTest {
     @Before
     public void setUp() {
         bank = new Bank();
-        bank.addAccount(AccountNumber.fromNumber(1), Amount.fromValue(0));
     }
 
     @Test(expected = AccountNotFoundException.class)
     public void should_throw_an_exception_when_account_is_not_found() throws Exception {
-        AccountNumber accountNumber = AccountNumber.fromNumber(5);
+        final AccountNumber accountNumber = AccountNumber.fromNumber(5);
+
         bank.consult(accountNumber);
     }
 
     @Test
     public void account_should_have_empty_history_by_default() throws Exception {
-        AccountNumber accountNumber = AccountNumber.fromNumber(1);
+        final AccountNumber accountNumber = AccountNumber.fromNumber(1);
+        final Amount amount = Amount.fromValue(0);
 
-        assertThat(bank.consult(accountNumber)).isEqualTo(Collections.singletonList(new CreationOperation(Amount.fromValue(0))));
+        bank.addAccount(accountNumber, amount);
+
+        assertThat(bank.consult(accountNumber)).isEqualTo(Collections.singletonList(new CreationOperation(amount)));
     }
 }
