b160a3e4ed6b531d9a98f0a76e349eb06e78ee00
Ubald de Bellabre <ubald.de-bellabre@lcdlv.fr>
2017-08-25 10:49:09 +0200

    [NEW] Acceptance test

---
+++ src/main/java/bankaccount/Account.java
@@ -0,0 +1,17 @@
+package bankaccount;
+
+import java.time.LocalDate;
+
+public class Account {
+    public void deposit(int amount, LocalDate operationDate) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void withdraw(int amount, LocalDate operationDate) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void history(Printer printer) {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ src/test/java/bankaccount/BankAccountFeature.java
@@ -0,0 +1,33 @@
+package bankaccount;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+public class BankAccountFeature {
+    private Printer printer;
+
+    @Before
+    public void setUp() {
+        printer = Mockito.mock(Printer.class);
+    }
+
+    @Test
+    public void deposit_and_withdrawal_on_accounts() {
+        Account account = new Account();
+
+        account.deposit(10, LocalDate.of(2017, Month.AUGUST, 25));
+        account.withdraw(5, LocalDate.of(2017, Month.AUGUST, 26));
+        account.deposit(50, LocalDate.of(2017, Month.AUGUST, 30));
+
+        account.history(printer);
+
+        Mockito.verify(printer).print("OPERATION | DATE | AMOUNT | BALANCE");
+        Mockito.verify(printer).print("Deposit | 25/08/2017 | 10€ | +10€");
+        Mockito.verify(printer).print("Withdraw | 26/08/2017 | 5€ | +5€");
+        Mockito.verify(printer).print("Deposit | 30/08/2017 | 50€ | +55€");
+    }
+}
