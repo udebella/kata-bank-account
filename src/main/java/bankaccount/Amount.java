1af97c7d8b8445cec0e7fc061407f3f516828f5a
Ubald de Bellabre <ubald.de-bellabre@lcdlv.fr>
2017-08-25 11:24:38 +0200

    [#1][NEW] Amounts can be compared

--- src/main/java/bankaccount/Amount.java
+++ src/main/java/bankaccount/Amount.java
@@ -1,7 +1,28 @@
 package bankaccount;
 
 public class Amount {
+    private final int value;
+
+    private Amount(int value) {
+        this.value = value;
+    }
+
     public static Amount of(int amount) {
-        return null;
+        return new Amount(amount);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Amount amount = (Amount) o;
+
+        return value == amount.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return value;
     }
 }
---
+++ src/test/java/bankaccount/AmountTest.java
@@ -0,0 +1,13 @@
+package bankaccount;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AmountTest {
+    @Test
+    public void amount_should_be_comparable() {
+        assertThat(Amount.of(10)).isEqualTo(Amount.of(10));
+        assertThat(Amount.of(0)).isNotEqualTo(Amount.of(10));
+    }
+}
