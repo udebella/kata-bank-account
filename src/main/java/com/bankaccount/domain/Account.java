547134c0fc907427d549ed950f09bf9adcc7bda0
Ubu <ubald2b@gmail.com>
2020-02-23 17:53:10 +0100

    Calculate balance for accounts with one deposit

--- src/main/java/com/bankaccount/domain/Account.java
+++ src/main/java/com/bankaccount/domain/Account.java
@@ -1,8 +1,22 @@
 package com.bankaccount.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Account {
+    private final List<Deposit> deposits = new ArrayList<>();
+
+    public Account() {}
+
+    public Account(Deposit deposit) {
+        this.deposits.add(deposit);
+    }
+
     public Balance balance() {
-        return new Balance(0);
+        return deposits.stream()
+                .reduce(new Balance(0),
+                        (balance, deposit) -> deposit.applyOn(balance),
+                        (balance, balance2) -> {throw new RuntimeException("Need implementation");});
     }
 
     public void deposit(PositiveAmount amount) {
--- src/test/java/com/bankaccount/domain/AccountTest.java
+++ src/test/java/com/bankaccount/domain/AccountTest.java
@@ -2,6 +2,8 @@ package com.bankaccount.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AccountTest {
@@ -12,5 +14,14 @@ public class AccountTest {
         final Balance balance = account.balance();
 
         assertThat(balance).isEqualTo(new Balance(0));
+    }
+
+    @Test
+    void should_calculate_balance_for_account_with_deposit() {
+        final Account account = new Account(new Deposit(PositiveAmount.of(5), LocalDate.now()));
+
+        final Balance balance = account.balance();
+
+        assertThat(balance).isEqualTo(new Balance(5));
     }
 }
