34ba6f5ac58dcd7c5003fde1b48644412a3faec9
Ubu <ubald2b@gmail.com>
2020-02-23 18:01:53 +0100

    Implement acceptance test for deposit

--- src/main/java/com/bankaccount/domain/Account.java
+++ src/main/java/com/bankaccount/domain/Account.java
@@ -1,13 +1,15 @@
 package com.bankaccount.domain;
 
-import java.util.Arrays;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Account {
     private final List<Deposit> deposits;
 
     public Account(Deposit... deposits) {
-        this.deposits = Arrays.asList(deposits);
+        this.deposits = Stream.of(deposits).collect(Collectors.toList());
     }
 
     public Balance balance() {
@@ -18,6 +20,7 @@ public class Account {
     }
 
     public void deposit(PositiveAmount amount) {
-
+        final Deposit deposit = new Deposit(amount, LocalDate.now());
+        this.deposits.add(deposit);
     }
 }
