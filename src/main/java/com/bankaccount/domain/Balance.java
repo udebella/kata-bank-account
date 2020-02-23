72225d36542d43d42308312ac3b02ad869e4f57f
Ubu <ubald2b@gmail.com>
2020-02-23 17:23:08 +0100

    Renamed internal representation of Balance

--- src/main/java/com/bankaccount/domain/Balance.java
+++ src/main/java/com/bankaccount/domain/Balance.java
@@ -3,14 +3,14 @@ package com.bankaccount.domain;
 import java.util.Objects;
 
 public final class Balance {
-    private final PositiveAmount balance;
+    private final PositiveAmount amount;
 
-    public Balance(long balance) {
-        this.balance = PositiveAmount.of(balance);
+    public Balance(long amount) {
+        this.amount = PositiveAmount.of(amount);
     }
 
     public Balance add(PositiveAmount amount) {
-        return balance.add(amount);
+        return this.amount.add(amount);
     }
 
     @Override
@@ -18,18 +18,18 @@ public final class Balance {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Balance balance1 = (Balance) o;
-        return Objects.equals(balance, balance1.balance);
+        return Objects.equals(amount, balance1.amount);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(balance);
+        return Objects.hash(amount);
     }
 
     @Override
     public String toString() {
         return "Balance{" +
-                "balance=" + balance +
+                "balance=" + amount +
                 '}';
     }
 }
