d01cec758b6127c9ba9dc09782606823a1eb13ef
Ubu <ubald2b@gmail.com>
2020-02-26 00:30:08 +0100

    Renamed operations in Account as it does not concern only deposits anymore

--- src/main/java/com/bankaccount/domain/Account.java
+++ src/main/java/com/bankaccount/domain/Account.java
@@ -14,16 +14,16 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Account {
-    private final List<Operation> deposits;
+    private final List<Operation> operations;
 
-    public Account(Operation... deposits) {
-        this.deposits = Stream.of(deposits).collect(Collectors.toList());
+    public Account(Operation... operations) {
+        this.operations = Stream.of(operations).collect(Collectors.toList());
     }
 
     public Balance balance() {
-        return deposits.stream()
+        return operations.stream()
                 .reduce(Balance.of(0),
-                        (balance, deposit) -> deposit.applyOn(balance),
+                        (balance, operation) -> operation.applyOn(balance),
                         (balance, balance2) -> {throw new RuntimeException("Need implementation");});
     }
 
@@ -37,7 +37,7 @@ public class Account {
 
     private void applyOperation(BiFunction<PositiveAmount, LocalDate, Operation> operationConstructor, PositiveAmount amount) {
         final Operation operation = operationConstructor.apply(amount, LocalDate.now());
-        this.deposits.add(operation);
+        this.operations.add(operation);
     }
 
     public void accept(AccountVisitor visitor) {
