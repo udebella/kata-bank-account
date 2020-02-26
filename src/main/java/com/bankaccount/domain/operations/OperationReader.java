f4c95ee9f664052dee4698e4e38573ea63da972d
Ubu <ubald2b@gmail.com>
2020-02-27 00:38:57 +0100

    Removed unused method in OperationReader interface

--- src/main/java/com/bankaccount/domain/operations/OperationReader.java
+++ src/main/java/com/bankaccount/domain/operations/OperationReader.java
@@ -5,8 +5,6 @@ import com.bankaccount.domain.money.Amount;
 import java.time.LocalDate;
 
 public interface OperationReader {
-    void readOperation(String operationType, Amount amount, LocalDate date);
-
     OperationReader readOperationType(String operationType);
 
     OperationReader readAmount(Amount amount);
