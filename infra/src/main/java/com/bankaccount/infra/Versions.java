23813aa68121f20dd8ba4e2c18d69b0776f8971e
Ubu <ubald2b@gmail.com>
2020-02-28 01:12:06 +0100

    Allow multiple versions

--- infra/src/main/java/com/bankaccount/infra/Versions.java
+++ infra/src/main/java/com/bankaccount/infra/Versions.java
@@ -1,13 +1,16 @@
 package com.bankaccount.infra;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Versions {
     private final List<String> versions;
 
     public Versions(int maxVersionNumber) {
-        this.versions = Collections.singletonList("/0");
+        this.versions = IntStream.rangeClosed(0, maxVersionNumber)
+                .mapToObj(version -> "/" + version)
+                .collect(Collectors.toList());
     }
 
     public List<String> getVersions() {
--- infra/src/test/java/com/bankaccount/infra/VersionsTest.java
+++ infra/src/test/java/com/bankaccount/infra/VersionsTest.java
@@ -13,10 +13,18 @@ import java.util.stream.Collectors;
 
 class VersionsTest {
     @Test
-    void should_serialize_properly() throws IOException, URISyntaxException, JSONException {
+    void should_serialize_properly_simple_version() throws IOException, URISyntaxException, JSONException {
         final String json = new ObjectMapper().writeValueAsString(new Versions(0));
 
         final String expected = Files.lines(Paths.get(getClass().getResource("/versions/simple-version.json").toURI())).collect(Collectors.joining());
-        JSONAssert.assertEquals(json, expected, true);
+        JSONAssert.assertEquals(expected, json, true);
+    }
+
+    @Test
+    void should_serialize_properly_multi_versions() throws IOException, URISyntaxException, JSONException {
+        final String json = new ObjectMapper().writeValueAsString(new Versions(2));
+
+        final String expected = Files.lines(Paths.get(getClass().getResource("/versions/multi-version.json").toURI())).collect(Collectors.joining());
+        JSONAssert.assertEquals(expected, json, true);
     }
 }
