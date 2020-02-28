d659ee38c531cd2ed68182ab79bc4598be61e47c
Ubu <ubald2b@gmail.com>
2020-02-28 01:25:18 +0100

    Implement list of versions returned by the repository

--- infra/src/main/java/com/bankaccount/infra/Controller.java
+++ infra/src/main/java/com/bankaccount/infra/Controller.java
@@ -1,13 +1,23 @@
 package com.bankaccount.infra;
 
+import com.bankaccount.domain.operations.Operation;
 import infra.Repository;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+
 public class Controller {
+    private final Repository repository;
+
     public Controller(Repository repository) {
+        this.repository = repository;
     }
 
     public ResponseEntity<?> versions() {
+        final List<Operation> operations = repository.operations();
+        if (!operations.isEmpty()) {
+            return ResponseEntity.ok(new Versions(operations.size()));
+        }
         return ResponseEntity.notFound().build();
     }
 }
--- infra/src/test/java/com/bankaccount/infra/ControllerTest.java
+++ infra/src/test/java/com/bankaccount/infra/ControllerTest.java
@@ -1,9 +1,12 @@
 package com.bankaccount.infra;
 
+import com.bankaccount.domain.operations.Deposit;
+import com.bankaccount.domain.operations.Operation;
 import infra.Repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.ResponseEntity;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,5 +23,16 @@ public class ControllerTest {
         final ResponseEntity<?> response = controller.versions();
 
         assertThat(response).isEqualTo(ResponseEntity.notFound().build());
+    }
+
+    @Test
+    void should_return_list_of_versions() {
+        final Repository repository = mock(Repository.class);
+        final Controller controller = new Controller(repository);
+        doReturn(Collections.singletonList(mock(Operation.class))).when(repository).operations();
+
+        final ResponseEntity<?> response = controller.versions();
+
+        assertThat(response).isEqualTo(ResponseEntity.ok(new Versions(1)));
     }
 }
