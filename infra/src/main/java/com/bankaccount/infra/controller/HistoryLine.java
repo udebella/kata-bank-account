8f0fa3616ff13216963f789e0d75c933976681dd
Ubu <ubald2b@gmail.com>
2020-03-01 16:36:35 +0100

    Check equality on HistoryLine

--- infra/src/main/java/com/bankaccount/infra/controller/HistoryLine.java
+++ infra/src/main/java/com/bankaccount/infra/controller/HistoryLine.java
@@ -2,8 +2,9 @@ package com.bankaccount.infra.controller;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
-public class HistoryLine {
+public final class HistoryLine {
     private final String operationType;
     private final String operationDate;
     private final long amount;
@@ -30,5 +31,31 @@ public class HistoryLine {
 
     public long getBalance() {
         return balance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        HistoryLine that = (HistoryLine) o;
+        return amount == that.amount &&
+                balance == that.balance &&
+                Objects.equals(operationType, that.operationType) &&
+                Objects.equals(operationDate, that.operationDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(operationType, operationDate, amount, balance);
+    }
+
+    @Override
+    public String toString() {
+        return "HistoryLine{" +
+                "operationType='" + operationType + '\'' +
+                ", operationDate='" + operationDate + '\'' +
+                ", amount=" + amount +
+                ", balance=" + balance +
+                '}';
     }
 }
--- infra/src/test/java/com/bankaccount/infra/controller/HistoryLineTest.java
+++ infra/src/test/java/com/bankaccount/infra/controller/HistoryLineTest.java
@@ -1,6 +1,7 @@
 package com.bankaccount.infra.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import nl.jqno.equalsverifier.EqualsVerifier;
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
@@ -14,6 +15,11 @@ import java.time.Month;
 import java.util.stream.Collectors;
 
 class HistoryLineTest {
+    @Test
+    void should_verify_equality() {
+        EqualsVerifier.forClass(HistoryLine.class).verify();
+    }
+
     @Test
     void should_serialize_properly() throws IOException, URISyntaxException, JSONException {
         final String json = new ObjectMapper().writeValueAsString(new HistoryLine("Deposit", LocalDate.of(2020, Month.MARCH, 1), 10, 10));
