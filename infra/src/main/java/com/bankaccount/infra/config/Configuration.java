75af41214deaf85a823e5718ddfbb449f248a597
Ubu <ubald2b@gmail.com>
2020-03-10 22:16:48 +0100

    Added swagger-ui

--- infra/src/main/java/com/bankaccount/infra/config/Configuration.java
+++ infra/src/main/java/com/bankaccount/infra/config/Configuration.java
@@ -5,11 +5,19 @@ import com.bankaccount.infra.repository.InMemoryRepository;
 import com.bankaccount.infra.repository.Repository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.time.LocalDate;
 
 @EnableWebMvc
-public class Configuration {
+@EnableSwagger2
+public class Configuration implements WebMvcConfigurer {
     @Bean
     public Controller controller(Repository repository) {
         return new Controller(repository, LocalDate::now);
@@ -18,5 +26,22 @@ public class Configuration {
     @Bean
     public Repository repository() {
         return new InMemoryRepository();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
